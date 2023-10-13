d8c57c82cc1a4c90aaeafcd862557e89a5cb7916
Mariia Tkachuk <mariya9424@gmail.com>
2023-10-13 21:55:51 +0300

    Second Homework add

---
+++ HW2 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+for (int i = -100; i < 101; i++)
+
+{
+    if (i == 100)
+    {
+        Console.WriteLine("Breaking the loop at i = 200");
+        
+    }
+    Console.WriteLine("Current value of i: " + i);
+        }
