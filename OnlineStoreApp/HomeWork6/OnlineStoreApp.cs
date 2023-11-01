c03c6e0378cf484abe776dbf21d602975acd4663
Mariia Tkachuk <mariya9424@gmail.com>
2023-11-01 22:43:56 +0200

    HomeWork 6

---
+++ OnlineStoreApp/HomeWork6/OnlineStoreApp.cs
@@ -0,0 +1,32 @@
+﻿public class OnlineStoreApp
+{
+    private ProductService productService;
+    private CartService cartService;
+    private OrderService orderService;
+
+    public OnlineStoreApp()
+    {
+        productService = new ProductService();
+        cartService = new CartService();
+        orderService = new OrderService();
+    }
+
+    public void Run()
+    {
+        Console.WriteLine("Welcome to the Online Store!");
+
+        var products = productService.GetProducts();
+
+    
+        cartService.AddToCart(products[1]);
+        cartService.AddToCart(products[3]);
+        cartService.AddToCart(products[7]);
+        cartService.AddToCart(products[6]);
+        cartService.AddToCart(products[10]);
+        cartService.AddToCart(products[5]);
+
+        var order = orderService.PlaceOrder(cartService.GetCartContents());
+
+        Console.WriteLine($"Order #{order.OrderNumber} has been generated.");
+    }
+}
